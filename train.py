52d8ceb4c27223f757ec1216c5f7cb2e25aaaafb
Oussama <61306430+elayeboussama@users.noreply.github.com>
2025-02-07 11:35:07 +0100

    Update train.py

--- train.py
+++ train.py
@@ -75,7 +75,7 @@ def get_runner_class(cfg):
 def main():
     # allow auto-dl completes on main process without timeout when using NCCL backend.
     # os.environ["NCCL_BLOCKING_WAIT"] = "1"
-    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:128"
+    os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:512"
 
     # set before init_distributed_mode() to ensure the same job_id shared across all ranks.
     job_id = now()
